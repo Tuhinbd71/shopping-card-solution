43e7d0de1aca51e9965ab649788387bcd73a0524
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-25 11:27:06 +0600

    case commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Shopping cart handles increase & decrease and update price using function
-function productQty(isIncreasing) {
+function iPhoneQty(isIncreasing) {
     const productIncrease = document.getElementById("product-qty");
     let productQty = productIncrease.value;
 
@@ -15,11 +15,37 @@ function productQty(isIncreasing) {
     iPhonePriceTotal.innerText = productQty * 1219;
 }
 document.getElementById("plus-btn").addEventListener("click", function () {
-    productQty(true);
+    iPhoneQty(true);
 })
 document.getElementById("minus-btn").addEventListener("click", function () {
-    productQty(false);
+    iPhoneQty(false);
 })
 
-// Case update section
+// Case increase & decrease and update price using function
+function caseQty(isIncrease) {
+    // case increase update
+    const caseQty = document.getElementById("case-qty");
+    const caseQtyValue = caseQty.value;
+    if (isIncrease == true) {
+        caseQty.value = parseInt(caseQtyValue) + 1;
+    }
+    else if (caseQtyValue > 0) {
+        caseQty.value = parseInt(caseQtyValue) - 1;
+    }
 
+}
+document.getElementById("case-plusBtn").addEventListener("click", function () {
+    // // case increase update
+    // const caseQty = document.getElementById("case-qty");
+    // const caseQtyValue = caseQty.value;
+    // caseQty.value = parseInt(caseQtyValue) + 1;
+    caseQty(true);
+})
+// case decrease update
+document.getElementById("case-minusBtn").addEventListener("click", function () {
+    // case increase update
+    // const caseQty = document.getElementById("case-qty");
+    // const caseQtyValue = caseQty.value;
+    // caseQty.value = parseInt(caseQtyValue) - 1;
+    caseQty(false);
+})
