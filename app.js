ea8fdc5907ddc1e4f81e60535a9664b80ac6a306
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-26 19:44:08 +0600

    product commit

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 // Shopping cart handles increase & decrease and update price using function
-function productQuentity(product, price, isIncreasing) {
-    const productIncrease = document.getElementById(product + "-qty");
-    let productQty = productIncrease.value;
+function updateProductInfo(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + "-qty");
+    let productQty = productInput.value;
     if (isIncreasing == true) {
         productQty = parseInt(productQty) + 1;
     }
     else if (productQty > 0) {
         productQty = parseInt(productQty) - 1;
     }
-    productIncrease.value = productQty;
+    productInput.value = productQty;
     // iPhone total price update section
-    const caseTotal = document.getElementById(product + "-total");
-    caseTotal.innerText = productQty * price;
-    // const iPhonePriceTotal = document.getElementById(product + "-total");
-    // iPhonePriceTotal.innerText = productQty * price;
-
+    const priceTotal = document.getElementById(product + "-total");
+    priceTotal.innerText = productQty * price;
 }
 // Phone increase & decrease and update price using function
 document.getElementById("phone-plusBtn").addEventListener("click", function () {
-    productQuentity("phone", 1219, true);
+    updateProductInfo("phone", 1219, true);
 });
 document.getElementById("phone-minusBtn").addEventListener("click", function () {
-    productQuentity("phone", 1219, false);
+    updateProductInfo("phone", 1219, false);
 });
-
 // Case increase & decrease and update price using function
 document.getElementById("case-plusBtn").addEventListener("click", function () {
-    productQuentity("case", 59, true);
+    updateProductInfo("case", 59, true);
 });
 document.getElementById("case-minusBtn").addEventListener("click", function () {
-    productQuentity("case", 59, false);
+    updateProductInfo("case", 59, false);
 });
 
