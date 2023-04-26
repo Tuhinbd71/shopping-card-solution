e83905e8c6f8ff56b45869b1628590bb8af5abcc
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-04-26 22:59:45 +0600

    total commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Shopping cart handles increase & decrease and update price using function
+// Shopping cart events handle increase & decrease and update price using function
 function updateProductInfo(product, price, isIncreasing) {
     const productInput = document.getElementById(product + "-qty");
     let productQty = productInput.value;
@@ -12,7 +12,27 @@ function updateProductInfo(product, price, isIncreasing) {
     // iPhone total price update section
     const priceTotal = document.getElementById(product + "-total");
     priceTotal.innerText = productQty * price;
+    // Update total
+    calculateTotal();
 }
+// Update total amount
+function getProductValue(product) {
+    const phoneQty = document.getElementById(product + "-qty");
+    const phoneQtyValue = parseInt(phoneQty.value);
+    return phoneQtyValue;
+}
+
+function calculateTotal() {
+    const phoneAmountTotal = getProductValue("phone") * 1219;
+    const caseAmountTotal = getProductValue("case") * 59;
+    const subTotalAmount = phoneAmountTotal + caseAmountTotal;
+    // Update on the html
+    const subTotal = document.getElementById("sub-total").innerText = subTotalAmount;
+
+}
+
+
+
 // Phone increase & decrease and update price using function
 document.getElementById("phone-plusBtn").addEventListener("click", function () {
     updateProductInfo("phone", 1219, true);
